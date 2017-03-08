3cc520ba0ae37e17c873649cfd628f7ef677e88d
Palmires <palmires@example.com>
2017-03-08 12:34:24 -0600

    Explanation added

--- WheteAmI_N3.go
+++ WheteAmI_N3.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	request := Request{agent: "Dee Fercloze", mission: "Operation Go"}
 
+	request.agent = "Β β aΒβ"
 	location := pingSattelite(request)
 	fmt.Println("Location:", location)
 }
@@ -22,15 +23,33 @@ func pingSattelite(request Request) string {
 	return revealLocation(isAccessible)
 }
 
+
+//So we need next:x=true,y=true,z=true.
 func accessible(agent string) bool {
 	a := strings.Split(agent, " ")
+	q := strings.Index("DeeFerclozej","eeFercloze")
+	fmt.Println(q)
 	if len(a) != 3 { return false }
 	b := a[0]
 	c := a[1]
 	d := a[2]
-	x := strings.EqualFold(b,c)
-	y := b != strings.ToLower(c)
+	// EqualFold reports whether b and c, interpreted as UTF-8 strings,
+	// are equal under Unicode case-folding.
+	x := strings.EqualFold(b,c) //There we had false
+	fmt.Println("X",x)
+	y := b != strings.ToLower(c) // There we had true. Compare two strings.
+	fmt.Println("Y",y)
+	// Index returns the index of the first instance of b+c in d, 
+	// or -1 if b+c is not present in d.
+	//
+	// What we need?
+	// First: three words in agent name.
+	// Second: Third word len=5
+	// Third: We need 2 string (b and c), that are equal under Unicode case-folding
+	// 			and differ in lowercase comparing.
+	//Additionally:unicode symbol had len=2
 	z := strings.Index(d, b+c) == 1 && len(d) == 5
+	fmt.Println("Z",z,d,b+c,len(d))
 	return x && y && z
 }
 
@@ -41,3 +60,7 @@ func revealLocation(access bool) string {
 		return "Failed to access. Good luck."
 	}
 }
+
+
+
+//Result request.agent = "Β β aΒβ"
